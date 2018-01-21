3830d0eda206c4ff4baff4f821be8f38c83dd45f
renee1027 <reneex.1027@gmail.com>
2018-01-21 02:16:07 +0100

    Add chat box

--- routes/randomStreamRoom.js
+++ routes/randomStreamRoom.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('randomStreamRoom', { title: 'Random Stream Room' });
+  res.render('randomStreamRoom', { 
+    title: 'Random Stream Room',
+    options: [{name: 'Twitter', icon: 'fa fa-twitter'}, 
+              {name: 'Facebook', icon: 'fa fa-facebook'},
+              {name: 'Polls', icon: 'fa fa-trophy'},
+              {name: 'Instagram', icon: 'fa fa-instagram'},
+              {name: 'Weibo', icon: 'fa fa-weibo'},
+              {name: 'Live Stats', icon: 'fa fa-line-chart'}]
+  });
 });
 
 module.exports = router;
